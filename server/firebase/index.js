2087578102ed3f5bce07112699eefcd5997e4049
kobol909 <campana.fabien@me.com>
2022-07-11 13:49:48 +0200

    Heroku: added GOOGLE_FIREBASE_CREDENTIALS in .env

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../.firebase/serviceAccountKey.json');
+// const serviceAccount = require('../.firebase/serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://react-native-firebase-app.firebaseio.com',
+  // credential: admin.credential.cert(serviceAccount),
+  credential: process.env.GOOGLE_FIREBASE_CREDENTIALS,
+  databaseURL: 'https://react-native-firebase-app.firebaseio.com'
 });
 
 module.exports = admin;
