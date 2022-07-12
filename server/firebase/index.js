e63dc6e081595fa5b453a0ad812906c3190cbbe9
kobol909 <campana.fabien@me.com>
2022-07-12 16:39:13 +0200

    Heroku: configuration...

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -2,7 +2,6 @@ const admin = require('firebase-admin');
 
 require('dotenv').config();
 const serviceAccount = JSON.parse(process.env.GOOGLE_FIREBASE_CREDENTIALS);
-console.log(serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
